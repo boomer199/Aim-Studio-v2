fc237024c7de029aa5a9fbfcc93774fd31595e0a
Ed <Ed>
2023-01-11 19:37:10 -0800

    [idk] :)

--- public/testing/testing.js
+++ public/testing/testing.js
@@ -1,7 +1,6 @@
 // Set up the canvas and get the canvas element
 const canvas = document.querySelector('#canvas');
 
-
 let player = {
     forward: false,
     backward: false,
@@ -11,10 +10,10 @@ let player = {
     zoomed: false,
     crouching: false,
     movementSpeed: 0.1,
-    sensitivity : 0.3,
+    sensitivity: 0.3,
     scoped_sensitivity: 0.15,
     boundingBox: new THREE.Box3(),
-    last_shot : 0,
+    last_shot: 0,
     score: 0,
     hitstreak: 1,
     lastKill: Date.now(),
@@ -27,13 +26,21 @@ const scene = new THREE.Scene();
 
 // Create some lighting
 const lights = [];
-lights[0] = new THREE.PointLight(0xffffff, 1, 0);
-lights[0].position.set(0, 10, 8);
+lights[0] = new THREE.PointLight(0xffffff, 0.5, 0);
+lights[0].position.set(-8, 28, 8);
+lights[1] = new THREE.PointLight(0xffffff, 0.5, 0);
+lights[1].position.set(8, 28, 8);
+lights[2] = new THREE.PointLight(0xffffff, 0.5, 0);
+lights[2].position.set(0, -0.3, -0.6);
 scene.add(lights[0]);
+scene.add(lights[1]);
 
 // Create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 scene.add(camera)
+
+camera.add(lights[2]);
+
 
 // Create a renderer
 const renderer = new THREE.WebGLRenderer({ canvas });
@@ -52,12 +59,23 @@ const targetBodyGeometry = new THREE.CapsuleGeometry(0.25, 0.875, 32, 32);
 const targetBodyMaterial = new THREE.MeshBasicMaterial({ color: 0xF0F000 });
 const targetBody = new THREE.Mesh(targetBodyGeometry, targetBodyMaterial);
 targetBody.position.y = 1.125;
+// create a capsule as the target's arms
+const targetArmGeometry = new THREE.CapsuleGeometry(0.15, 0.5, 32, 32);
+const targetArmMaterial = new THREE.MeshBasicMaterial({ color: 0x00FF00 });
+const targetArmLeft = new THREE.Mesh(targetArmGeometry, targetArmMaterial);
+targetArmLeft.position.y = 1.3;
+const targetArmRight = new THREE.Mesh(targetArmGeometry, targetArmMaterial);
+targetArmRight.position.y = 1.3;
+
+scene.add(targetBody);
+scene.add(targetHead);
+scene.add(targetArmLeft);
+scene.add(targetArmRight);
 
 let health = 3
 
 //Create a floor
-const planeGeometry = new THREE.PlaneGeometry(20, 20, 1, 1);
-const floorGeometry = new THREE.PlaneGeometry(100, 100, 1, 1);
+const planeGeometry = new THREE.BoxGeometry(20, 20, 0.1, 1, 1);
 const planeMaterial = new THREE.MeshPhongMaterial({ color: 0x156289, emissive: 0x072534, side: THREE.DoubleSide, flatShading: true });
 const floor = new THREE.Mesh(planeGeometry, planeMaterial);
 floor.rotation.x = Math.PI / 2;
@@ -66,8 +84,6 @@ camera.position.z = 8;
 camera.position.y = 2;
 camera.lookAt(targetHead.position);
 
-scene.add(targetBody);
-scene.add(targetHead);
 scene.add(camera);
 scene.add(floor);
 scene.background = 0x444444;
@@ -75,31 +91,45 @@ scene.background = 0x444444;
 // Create a front wall using the plane geometry and material
 const frontWall = new THREE.Mesh(planeGeometry, planeMaterial);
 frontWall.position.set(0, 10, -10);
+let frontWallBB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+frontWallBB.setFromObject(frontWall);
 
 // Create a right wall using the plane geometry and material
 const rightWall = new THREE.Mesh(planeGeometry, planeMaterial);
 rightWall.position.set(10, 10, 0);
 rightWall.rotation.y = Math.PI / 2;
+let rightWallBB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+rightWallBB.setFromObject(frontWall);
 
 // Create a left wall using the plane geometry and material
 const leftWall = new THREE.Mesh(planeGeometry, planeMaterial);
 leftWall.position.set(-10, 10, 0);
 leftWall.rotation.y = Math.PI / 2;
+let leftWallBB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+leftWallBB.setFromObject(frontWall);
 
 // Create a back wall using the plane geometry and material
 const backWall = new THREE.Mesh(planeGeometry, planeMaterial);
 backWall.position.set(0, 10, 10);
+let backWallBB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+backWallBB.setFromObject(frontWall);
 
 // Create a roof using the plane geometry and material
 const roof = new THREE.Mesh(planeGeometry, planeMaterial);
 roof.position.set(0, 20, 0);
 roof.rotation.x = Math.PI / 2;
+let roofBB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+roofBB.setFromObject(frontWall);
 
 const smallWallGeometry = new THREE.BoxGeometry(20, 1.5, 1);
 const smallWall = new THREE.Mesh(smallWallGeometry, planeMaterial);
 smallWall.position.set(0, 0.75, 7)
 scene.add(smallWall);
+let smallWallBB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+smallWallBB.setFromObject(frontWall);
 
+let bullets = [];
+let trails = [];
 
 walls = [frontWall, leftWall, rightWall, backWall, smallWall, roof]
 
@@ -132,7 +162,7 @@ document.addEventListener('exitpointerlock', function () {
 });
 
 // Create a crosshair geometry
-const crosshairGeometry = new THREE.SphereGeometry(0.0025, 32,32);
+const crosshairGeometry = new THREE.SphereGeometry(0.0025, 32, 32);
 const crosshairMaterial = new THREE.MeshBasicMaterial({ color: 0x00cc00 });
 const crosshair = new THREE.Mesh(crosshairGeometry, crosshairMaterial);
 
@@ -147,15 +177,15 @@ var objLoader = new THREE.OBJLoader();
 
 
 var weaponLeft = undefined;
-var bullet = undefined;
-mtlLoader.load("./Models/pistol.mtl", function(materials) {
+
+mtlLoader.load("Models/sniperCamo.mtl", function (materials) {
     materials.preload();
     objLoader.setMaterials(materials);
-    objLoader.load("./Models/pistol.obj", function(object) {
+    objLoader.load("./Models/sniperCamo.obj", function (object) {
         weaponLeft = object;
         weaponLeft.rotation.y = Math.PI;
         weaponLeft.scale.set(3.5, 3.5, 3.5)
-        weaponLeft.position.set(0.25, -0.3, -0.45);
+        weaponLeft.position.set(0.25, -0.3, -0.5);
         camera.add(weaponLeft);
     });
 });
@@ -164,22 +194,24 @@ mtlLoader.load("./Models/pistol.mtl", function(materials) {
 // Update the position of the crosshair on each frame
 function updateCrosshairSize() {
     // Set the camera direction based on the camera rotation
-  
+
     // Set the crosshair position to be a fixed distance away from the camera
-    if (!player.zoomed){
-         // Set the crosshair position to be 0.5 units away from the camera
+    if (!player.zoomed) {
+        // Set the crosshair position to be 0.5 units away from the camera
         crosshair.position.z = -0.5;
     }
-    if (player.zoomed){
+    if (player.zoomed) {
         // Set the crosshair position to be -1.25 units away from the camera
-       crosshair.position.z = -1.25;
-   }
+        crosshair.position.z = -1.25;
+    }
 }
 var bang = new Audio('sounds/sniper.mp3');
 var bonk = new Audio('sounds/bonk.mp3');
 
+const trailMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff });
+
 // Check if the crosshair is intersecting the target on each mouse click (shooting) and zoom on right click
-document.addEventListener('mousedown', function(click) {
+document.addEventListener('mousedown', function (click) {
     switch (click.button) {
         case 2:
             if (!player.zoomed) {
@@ -208,36 +240,54 @@ document.addEventListener('mousedown', function(click) {
             raycaster.set(camera.position, cameraDirection);
 
             // Cast a ray from the camera and get the intersecting objects
-            const Hit = raycaster.intersectObjects([targetHead, targetBody, floor, smallWall, leftWall, rightWall, frontWall, backWall, roof]);
+            const Hit = raycaster.intersectObjects([targetHead, targetBody, targetArmLeft, targetArmRight, floor, smallWall, leftWall, rightWall, frontWall, backWall, roof]);
 
+            let bulletColor = 0xffffff
+            hitNumber = 0
             // If the crosshair is intersecting the target, remove the target from the scene
-            if (Hit[0].object == floor || 
-                    Hit[0].object == smallWall || 
-                    Hit[0].object == leftWall || 
-                    Hit[0].object == rightWall || 
-                    Hit[0].object == frontWall || 
-                    Hit[0].object == backWall || 
-                    Hit[0].object == roof) {
-                console.log("wall");
+            if (Hit[0].object == floor ||
+                Hit[0].object == smallWall ||
+                Hit[0].object == leftWall ||
+                Hit[0].object == rightWall ||
+                Hit[0].object == frontWall ||
+                Hit[0].object == backWall ||
+                Hit[0].object == roof) {
                 bonk.load();
-                bonk.play();   
+                bonk.play();
+                bang.load();
+                bang.play();
                 player.hitstreak = 0;
                 player.score -= 20;
             }
             else if (Hit[0].object == targetHead) {
                 health -= 3;
                 bang.load();
-                bang.play();    
+                bang.play();
+                bulletColor = 0xff0000
+                hitNumber += 1
             }
             else if (Hit[0].object == targetBody) {
                 health -= 1;
                 bang.load();
-                bang.play();   
+                bang.play();
+                bulletColor = 0xffff00
+                hitNumber += 1
+            }
+            else if (Hit[0].object == targetArmLeft || Hit[0].object == targetArmRight) {
+                console.log("arm")
+                health -= 0.6;
+                bang.load();
+                bang.play();
+                bulletColor = 0x00ff00
+                hitNumber += 1
             }
 
+            if ((Hit[1].object == targetBody && Hit[0].object == targetHead) || (Hit[0].object == targetBody && Hit[1].object == targetHead) || (Hit[0].object == targetBody && Hit[1].object == targetArmLeft) || (Hit[0].object == targetBody && Hit[1].object == targetArmRight) || (Hit[0].object == targetBody && Hit[1].object == targetHead) || (Hit[0].object == targetArmLeft && Hit[1].object == targetBody) || (Hit[0].object == targetArmRight && Hit[1].object == targetBody)) {
+                hitNumber += 1
+            }
 
             if (health <= 0) {
-                player.hitstreak++; 
+                player.hitstreak++;
                 scene.remove(targetHead);
                 scene.remove(targetBody);
                 targetHead.position.x = Math.random() * 6 - 3;
@@ -248,22 +298,69 @@ document.addEventListener('mousedown', function(click) {
                 scene.add(targetBody);
                 health = 3;
 
-                if (player.score + 100 > player.score + (player.hitstreak * 0.37623) * 100 - ((Date.now() - player.lastKill)/70)){
+                if (player.score + 100 > player.score + (player.hitstreak * 0.37623) * 100 - ((Date.now() - player.lastKill) / 70)) {
                     player.score += 100;
                 } else {
-                    player.score += (player.hitstreak * 0.37623) * 100 - ((Date.now() - player.lastKill)/70)
+                    player.score += (player.hitstreak * 0.37623) * 100 - ((Date.now() - player.lastKill) / 70)
                 }
                 player.lastKill = Date.now();
                 console.log("Score: " + player.score)
             }
             player.last_shot = Date.now();
+
+            var bullet = new THREE.Mesh(new THREE.SphereGeometry(0.05, 32, 32), new THREE.MeshBasicMaterial({ color: bulletColor }));
+
+            bullet.alive = true;
+            bullet.velocity = cameraDirection;
+            bullet.position.set(Hit[hitNumber].point.x, Hit[hitNumber].point.y, Hit[hitNumber].point.z)
+            scene.add(bullet);
+            bullets.push(bullet);
+
+            const points = [];
+            points.push(new THREE.Vector3(camera.position.x, camera.position.y, camera.position.z));
+            points.push(new THREE.Vector3(Hit[hitNumber].point.x, Hit[hitNumber].point.y, Hit[hitNumber].point.z));
+            const trailMaterial = new THREE.LineBasicMaterial({ color: bulletColor });
+            const trailGeometry = new THREE.BufferGeometry().setFromPoints(points);
+            const trail = new THREE.Line(trailGeometry, trailMaterial);
+            scene.add(trail);
+            setTimeout(function () {
+                bullet.alive = false;
+                scene.remove(bullet);
+            }, 5000);
+            setTimeout(function () {
+                trail.alive = false;
+                scene.remove(trail);
+            }, 1000)
+
             break;
         default:
             console.log(`Unknown button code: ${e.button}`);
     }
 });
 
-document.addEventListener('keydown', function(event) {
+// function updateBulletPositions() {
+//     for (var i = 0; i < bullets.length; i += 1) {
+//         if (bullets[i] === undefined) continue;
+//         if (bullets[i].alive == false) {
+//             bullets.splice(i, 1)
+//             continue;
+//         }
+//         bullets[i].position.y -= 0.01;
+//         bullets[i].position.add(new THREE.Vector3(bullets[i].velocity.x*1.5, bullets[i].velocity.y*1.5, bullets[i].velocity.z*1.5))
+//     }
+// }
+function updateTrailVisiblity() {
+    for (var i = 0; i < trails.length; i += 1) {
+        if (trails[i] === undefined) continue;
+        if (trails[i].alive == false) {
+            trails.splice(i, 1)
+            scene.remove(trails[i])
+            continue;
+        }
+    }
+}
+
+document.addEventListener('keydown', function (event) {
     switch (event.key) {
         case "w":
         case "W":
@@ -294,7 +391,7 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
-document.addEventListener('keyup', function(event) {
+document.addEventListener('keyup', function (event) {
     switch (event.key) {
         case "w":
         case "W":
@@ -370,22 +467,22 @@ function movePlayer() {
     }
 }
 
-function updatePlayerWalls() {    
+function updatePlayerWalls() {
     // Check if the player has reached the front wall
     if (camera.position.z <= 7.8) {
         camera.position.z = 7.8;
     }
-    
+
     // Check if the player has reached the right wall
     if (camera.position.x >= 9.8) {
         camera.position.x = 9.8
     }
-    
+
     // Check if the player has reached the left wall
     if (camera.position.x <= -9.8) {
         camera.position.x = -9.8
     }
-    
+
     // Check if the player has reached the back wall
     if (camera.position.z >= 9.8) {
         camera.position.z = 9.8
@@ -397,17 +494,16 @@ let targetSmooth = false;
 let targetSmoothNum = 0;
 
 function moveTarget() {
-    
-    scene.remove(targetHead);
-    scene.remove(targetBody);
 
-    if(targetSmooth){
+    if (targetSmooth) {
         targetHead.position.x += Math.random() * 0.0625;
         targetBody.position.x = targetHead.position.x;
         targetBody.position.z = targetHead.position.z;
-        scene.add(targetHead)
-        scene.add(targetBody)
-        if(targetSmoothNum != 60){
+        targetArmLeft.position.x = targetHead.position.x-0.25;
+        targetArmLeft.position.z = targetHead.position.z;
+        targetArmRight.position.x = targetHead.position.x+0.25;
+        targetArmRight.position.z = targetHead.position.z;
+        if (targetSmoothNum != 60) {
             targetSmoothNum++;
         } else {
             targetSmooth = false;
@@ -417,17 +513,17 @@ function moveTarget() {
         targetHead.position.x -= Math.random() * 0.0625;
         targetBody.position.x = targetHead.position.x;
         targetBody.position.z = targetHead.position.z;
-        scene.add(targetHead)
-        scene.add(targetBody)
-        if(targetSmoothNum != 60){
+        targetArmLeft.position.x = targetHead.position.x-0.25;
+        targetArmLeft.position.z = targetHead.position.z;
+        targetArmRight.position.x = targetHead.position.x+0.25;
+        targetArmRight.position.z = targetHead.position.z;
+        if (targetSmoothNum != 60) {
             targetSmoothNum++;
         } else {
             targetSmooth = true;
             targetSmoothNum = 0;
         }
     }
-
-    
 }
 
 
@@ -438,13 +534,14 @@ function animate() {
     movePlayer();
     updatePlayerWalls();
     updateCrosshairSize();
+    updateTrailVisiblity();
     renderer.setClearColor(0x808080);
     renderer.render(scene, camera);
 }
 
 
 //setting sens
-controls.pointerSpeed = player.sensitivity; 
+controls.pointerSpeed = player.sensitivity;
 
 //init
 animate();
